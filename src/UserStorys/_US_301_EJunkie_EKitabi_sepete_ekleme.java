e1213b58ca921d9b9f9a582c7eabbd76e062d0e3
alev91 <alevkonya91@gmail.com>
2024-03-17 14:07:00 +0300

    add to cart buttonuna tıklandı, kitap sepete eklendi

--- src/UserStorys/_US_301_EJunkie_EKitabi_sepete_ekleme.java
+++ src/UserStorys/_US_301_EJunkie_EKitabi_sepete_ekleme.java
@@ -1,6 +1,21 @@
 package UserStorys;
 
 import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class _US_301_EJunkie_EKitabi_sepete_ekleme extends BaseDriver {
+
+    @Test
+    public void EKitabiSepeteEkleme(){
+
+     driver.get("https://shopdemo.e-junkie.com/");
+
+     WebElement addToCardBtn= driver.findElement(By.cssSelector("button[onclick=\"return EJProductClick('1595015')\"]"));
+     addToCardBtn.click();
+
+
+        BekleKapat();
+    }
 }
